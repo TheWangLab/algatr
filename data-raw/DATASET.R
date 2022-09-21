72b20bf954577e6f283b392e4bcc085b698c1bba
E. Anne Chambers <eachambers@utexas.edu>
2022-09-21 14:37:02 -0400

    update liz_gendist dataset

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,8 +1,8 @@
 # Code to create example data -------------------------------------------------------------
 
-# load and save vcf subset
+# Load and save vcf subset
 liz_vcf <- vcfR::read.vcfR("inst/extdata/populations_r20.haplotypes.filtered_m70_randomSNP.vcf")
-# subset out 1000 SNPs
+# Subsample 1000 SNPs
 liz_vcf <- liz_vcf[1:1000,]
 
 # Get CA shapefile
@@ -15,39 +15,39 @@ states <- sf::as_Spatial(states)
 # Subset out CA
 CA <- states[which(states$NAME == "California"), "STUSPS"]
 
-# load and save coords
+# Load and save coords
 liz_coords <- read.table("inst/extdata/Scelop.coord")
-# rename cols
+# Rename cols
 colnames(liz_coords) <- c("x", "y")
-# get sample IDs from vcf data
+# Get sample IDs from vcf data
 liz_coords$ID <- colnames(liz_vcf@gt)[-1]
-# create spatial coordinates
+# Create spatial coordinates
 sp::coordinates(liz_coords) <- ~x+y
-# add CRS
+# Add CRS
 raster::crs(liz_coords) <- raster::crs(CA)
-# only include coordinates within CA
+# Only include coordinates within CA
 liz_coords <- liz_coords[CA,]
-# get IDS of coordinates within CA (to use to subset VCF)
+# Get IDS of coordinates within CA (to use to subset VCF)
 IDS <- liz_coords$ID
-# create dataframe
+# Create dataframe
 liz_coords <- data.frame(liz_coords)
-# only keep x and y
+# Only keep x and y
 liz_coords <- liz_coords[,c("x","y")]
 usethis::use_data(liz_coords, overwrite = TRUE)
 
-# subset vcf to match coords
+# Subset vcf to match coords
 index <- colnames(liz_vcf@gt) %in% IDS
-# first col is format col
+# First col is format col
 index[1] <- TRUE
-# subset vcf to match coords
+# Subset vcf to match coords
 liz_vcf <- liz_vcf[ , index]
 usethis::use_data(liz_vcf, overwrite = TRUE)
 
-# check IDs match (remember first col is format)
+# Check IDs match (remember first col is format)
 stopifnot(colnames(liz_vcf@gt)[-1] == IDS)
 
 
-# load env data
+# Load env data
 CA_env <- raster::stack(list.files("inst/extdata/PC_layers/", full.names = TRUE))
 raster::writeRaster(CA_env, "inst/extdata/CA_env.tif", overwrite = TRUE)
 CA_env <- raster::readAll(CA_env)
@@ -57,15 +57,13 @@ usethis::use_data(CA_env, overwrite = TRUE)
 dos <- dos[complete.cases(dos),]
 prcomp(~., data.frame(dos))
 
-# process plink genetic distances
-plink_file = here("data", "liz_test.dist")
-plink_id_file = here("data", "liz_test.dist.id")
-gendist <- as.data.frame(readr::read_tsv(plink_file, col_names = FALSE))
-plink_names <- readr::read_tsv(plink_id_file, col_names = FALSE) %>%
+# Process plink genetic distances
+liz_gendist <- as.data.frame(readr::read_tsv("inst/extdata/liz_test.dist", col_names = FALSE))
+plink_names <- readr::read_tsv("inst/extdata/liz_test.dist.id", col_names = FALSE) %>%
   dplyr::select(-`X1`) %>%
   as.matrix()
 
 # Assign row and col names according to sampleID
-rownames(gendist) <- plink_names
-colnames(gendist) <- plink_names
-usethis::use_data(gendist, overwrite = TRUE)
+rownames(liz_gendist) <- plink_names
+colnames(liz_gendist) <- plink_names
+usethis::use_data(liz_gendist, overwrite = TRUE)
