fc9e0fbd8b744a937ea04ee03080d3cebe068f8f
E. Anne Chambers <eachambers@utexas.edu>
2022-10-26 16:24:28 -0500

    edits to gen dist and masking vignettes

--- R/Gen_dist.R
+++ R/Gen_dist.R
@@ -2,19 +2,20 @@
 #' Calculate genetic distances
 #'
 #' @param vcf path to vcf file or a `vcfR` type object
+#' @param dist_type the type of genetic distance to calculate
 #' @param plink_file path to plink distance file (typically ".dist"; required only for calculating plink distance)
 #' @param plink_id_file path to plink id file (typically ".dist.id"; required only for calculating plink distance)
-#' @param dist_type the type of genetic distance to calculate (TODO[EAC]: Switch the order so that dist_type comes before the plink files since dist_type is always required but plink files are not)
-#' @param criticalpoint the critical point for the significance threshold for the Tracy Widom test within the PCA used to determine the number of PCs automatically (TODO[EAC]: add argument for automatic vs manual selection of PCs (e.g. have the function print a scree plot and then users can select a number of PCs and also have an argument for this function that is nPC ( i think I have this added in TESS)))
+#' @param criticalpoint the critical point for the significance threshold for the Tracy-Widom test within the PCA used to determine the number of PCs automatically (TODO[EAC]: add argument for automatic vs manual selection of PCs (e.g. have the function print a scree plot and then users can select a number of PCs and also have an argument for this function that is nPC ( i think I have this added in TESS)))
 #'
 #' @details
 #' Euclidean and Bray-Curtis distances calculated using the ecodist package: Goslee, S.C. and Urban, D.L. 2007. The ecodist package for dissimilarity-based analysis of ecological data. Journal of Statistical Software 22(7):1-19. DOI:10.18637/jss.v022.i07.
 #' Proportions of shared alleles calculated using the adegenet package: Jombart T. and Ahmed I. (2011) adegenet 1.3-1: new tools for the analysis of genome-wide SNP data. Bioinformatics. doi:10.1093/bioinformatics/btr521.
+#' For calculating proportions of shared alleles, missing values are ignored (i.e., prop shared alleles calculated from present values; no scaling performed)
 #' For PC-based distances, mean-based imputation calculated using the dartR package: Gruber, B, Unmack, PJ, Berry, OF, Georges, A. dartr: An r package to facilitate analysis of SNP data generated from reduced representation genome sequencing. Mol Ecol Resour. 2018; 18: 691-699. https://doi.org/10.1111/1755-0998.12745
 
 #' @return pairwise distance matrix for given distance metric
 #'
-gen_dist <- function(vcf = NULL, plink_file = NULL, plink_id_file = NULL, dist_type, criticalpoint = 2.0234){
+gen_dist <- function(vcf = NULL, dist_type, plink_file = NULL, plink_id_file = NULL, criticalpoint = 2.0234){
 
   # Import vcf if provided --------------------------------------------------
 
@@ -31,12 +32,6 @@ gen_dist <- function(vcf = NULL, plink_file = NULL, plink_id_file = NULL, dist_t
     if(any(is.na(mat))){
       mat <- simple_impute(mat, median)
       warning("NAs found in genetic data, imputing to the median (NOTE: this simplified imputation approach is strongly discouraged. Consider using another method of removing missing data)")
-    }
-
-    # Check for NAs
-    # TODO[EAC]: this code is repetitive with the above expression
-    if(any(is.na(mat))){
-      stop("NA values found in genetic data")
     }
 
     dists <- ecodist::distance(mat, method = "euclidean")
@@ -72,7 +67,6 @@ gen_dist <- function(vcf = NULL, plink_file = NULL, plink_id_file = NULL, dist_t
   if (dist_type == "dps") {
     # Convert to genind
     genind <- vcfR::vcfR2genind(vcf)
-    # TODO[EAC]: include in function description how adegent deals with NAs?
     dists <- adegenet::propShared(genind)
     return(as.data.frame(dists))
   }
