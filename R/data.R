fb1bbe2d19b08ec0825b0b212e087227269b4a5f
E. Anne Chambers <eachambers@utexas.edu>
2022-11-14 14:38:23 -0800

    fixes to RDA code and vignette

--- R/data.R
+++ R/data.R
@@ -12,4 +12,14 @@
 #' @source \url{https://doi.org/10.1111/mec.15836}
 "liz_coords"
 
-# TODO [EAC]: gendist and CA_env need to be added here?
+#' Example genetic distance matrix, calculated with Plink using data from Bouzid et al. 2022
+#'
+#' @format A dataframe with genetic distances for 53 samples
+#'
+#' @source \url{https://doi.org/10.1111/mec.15836}
+"liz_gendist"
+
+#' Example environmental data, calculated by performing a raster PCA on 18 bioclimatic variables for state of California
+#'
+#' @format A RasterStack object with three environmental PCs
+"CA_env"
