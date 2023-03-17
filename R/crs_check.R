3375e1b4e55fa47e6aa79d569042bd9a50cb1f48
Anusha Bishop <46795148+AnushaPB@users.noreply.github.com>
2023-03-16 18:17:33 -0700

    Update crs_check.R

--- R/crs_check.R
+++ R/crs_check.R
@@ -14,11 +14,11 @@ crs_check <- function(coords, lyr = NULL) {
   # get CRS
   coords_crs <- sf::st_crs(coords)
 
-  if (is.na(coords_crs)) warning("No CRS found for the provided coordinates. Make sure the coordinates and the raster have the same projection (see function details or wingen vignette)")
+  if (is.na(coords_crs)) warning("No CRS found for the provided coordinates. Make sure the coordinates and the raster have the same projection (see function details or vignette)")
 
   if (!is.null(lyr)){
     lyr_crs <- sf::st_crs(lyr)
-    if (is.na(lyr_crs)) warning("No CRS found for the provided raster. Make sure the coordinates and the raster have the same projection (see function details or wingen vignette)")
+    if (is.na(lyr_crs)) warning("No CRS found for the provided raster. Make sure the coordinates and the raster have the same projection (see function details or vignette)")
 
     if (!is.na(lyr_crs) & !is.na(coords_crs)) {
       if (coords_crs != lyr_crs) stop("CRS of the provided coordinates and raster do not match")
