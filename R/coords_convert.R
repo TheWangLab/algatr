a4d75516ac6e8c3d9a0934426147e0a4ece50ea8
Anusha Bishop <46795148+AnushaPB@users.noreply.github.com>
2023-03-16 22:17:05 -0700

    add coords to matrix

--- R/coords_convert.R
+++ R/coords_convert.R
@@ -8,7 +8,7 @@ coords_to_sf <- function(coords){
   return(coords)
 }
 
-#  convert from matrix/data.frame/sf to sp
+#  convert from matrix/data.frame/sf to formatted sp
 coords_to_sp <- function(coords){
   coords <- coords_to_sf(coords)
   coords <- sf::as_Spatial(coords)
@@ -17,10 +17,17 @@ coords_to_sp <- function(coords){
   return(coords)
 }
 
-# convert from matrix/data.frame/sf to df
+# convert from matrix/data.frame/sf to formatted df
 coords_to_df <- function(coords){
   if (inherits(coords, "sf")) coords <- data.frame(coords_to_sp(coords)) %>% dplyr::select(-optional)
   if (is.matrix(coords)) coords <- data.frame(coords)
   colnames(coords) <- c("x", "y")
+  return(coords)
+}
+
+# convert from matrix/data.frame/sf to formatted matrix
+coords_to_matrix <- function(coords){
+  coords <- coords_to_df(coords)
+  coords <- as.matrix(coords)
   return(coords)
 }
