ee5bf672651b14e6d9b2373a015b669c2e2aa679
Anusha Bishop <46795148+AnushaPB@users.noreply.github.com>
2023-03-16 18:17:30 -0700

    Update coords_convert.R

--- R/coords_convert.R
+++ R/coords_convert.R
@@ -16,3 +16,11 @@ coords_to_sp <- function(coords){
   colnames(coords@coords) <- c("x", "y")
   return(coords)
 }
+
+# convert from matrix/data.frame/sf to df
+coords_to_df <- function(coords){
+  if (inherits(coords, "sf")) coords <- data.frame(coords_to_sp(coords)) %>% dplyr::select(-optional)
+  if (is.matrix(coords)) coords <- data.frame(coords)
+  colnames(coords) <- c("x", "y")
+  return(coords)
+}
