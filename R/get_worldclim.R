5805c5fe3f401ceee7ce0cfb637e5cc6fc0ab732
Anusha Bishop <46795148+AnushaPB@users.noreply.github.com>
2023-03-16 22:35:31 -0700

    Update get_worldclim.R

--- R/get_worldclim.R
+++ R/get_worldclim.R
@@ -14,18 +14,23 @@
 #'
 #' @examples
 get_worldclim <- function(coords, res = 0.5, buff = 0.01, save_output = FALSE){
-  # Raster of worldclim tiles
-  r <- raster::raster(vals = 1:60, nrows = 5, ncols = 12, ext = raster::extent(c(-180, 180, -90, 90)))
 
-  # Make SpatialPolygons object with convex hull of coords
+  # Convert coordinates
+  coords <- coords_to_df(coords)
+
+  # Raster of worldclim tiles
+  r <- terra::rast(vals = 1:60, nrows = 5, ncols = 12, ext = raster::extent(c(-180, 180, -90, 90)))
+
+  # Make sf object with convex hull of coords
   ch_pts <- grDevices::chull(coords)
   ch_poly <- sp::Polygon(coords[ch_pts,])
   ch_polys <- sp::Polygons(list(ch_poly), ID = "chull")
   ch_spolys <- sp::SpatialPolygons(list(ch_polys))
+  ch_sf <- sf::st_as_sf(ch_spolys)
 
   # Identify WorldClim tiles to download
-  r_nums <- raster::extract(r, ch_spolys)
-  r_xy <- raster::xyFromCell(r, r_nums[[1]])
+  r_nums <- terra::extract(r, ch_sf, ID = FALSE)
+  r_xy <- terra::xyFromCell(r, r_nums[[1]])
 
   # Download and merge tiles
   message("Downloading WorldClim tile 1...")
@@ -35,46 +40,44 @@ get_worldclim <- function(coords, res = 0.5, buff = 0.01, save_output = FALSE){
     for(i in 2:length(r_nums[[1]])){
       message(paste0("Downloading WorldClim tile ", i, "..."))
       wc <- geodata::worldclim_tile(var = "bio", lon = r_xy[i, 1], lat = r_xy[i, 2], path = folder)
-      wclim <- raster::merge(wclim, wc)
+      wclim <- terra::merge(wclim, wc)
     }
   }
 
   # Define crop area based on buffer size
-  colnames(coords) <- c("x", "y")
-  buff_ext <- raster::extent(coords)
+  buff_ext <- as.vector(terra::ext(coords_to_sf(coords)))
   ext_vals <- c()
-  if(buff_ext@xmin < 0){
-    ext_vals[1] <- buff_ext@xmin * (1 + buff)
+  if(buff_ext["xmin"] < 0){
+    ext_vals[1] <- buff_ext["xmin"] * (1 + buff)
   } else {
-    ext_vals[1] <- buff_ext@xmin * (1 - buff)
+    ext_vals[1] <- buff_ext["xmin"] * (1 - buff)
   }
-  if(buff_ext@xmax < 0){
-    ext_vals[2] <- buff_ext@xmax * (1 - buff)
+  if(buff_ext["xmax"] < 0){
+    ext_vals[2] <- buff_ext["xmax"] * (1 - buff)
   } else {
-    ext_vals[2] <- buff_ext@xmax * (1 + buff)
+    ext_vals[2] <- buff_ext["xmax"] * (1 + buff)
   }
-  if(buff_ext@ymin < 0){
-    ext_vals[3] <- buff_ext@ymin * (1 + buff)
+  if(buff_ext["ymin"] < 0){
+    ext_vals[3] <- buff_ext["ymin"] * (1 + buff)
   } else {
-    ext_vals[3] <- buff_ext@ymin * (1 - buff)
+    ext_vals[3] <- buff_ext["ymin"] * (1 - buff)
   }
-  if(buff_ext@ymax < 0){
-    ext_vals[4] <- buff_ext@ymax * (1 - buff)
+  if(buff_ext["ymax"]  < 0){
+    ext_vals[4] <- buff_ext["ymax"] * (1 - buff)
   } else {
-    ext_vals[4] <- buff_ext@ymax * (1 + buff)
+    ext_vals[4] <- buff_ext["ymax"] * (1 + buff)
   }
-  buff_ext <- raster::extent(ext_vals)
+  buff_ext <- terra::ext(ext_vals)
 
   # Crop raster stack to buffered area
-  wclim <- raster::crop(wclim, buff_ext)
+  wclim <- terra::crop(wclim, buff_ext)
 
   # Assign names to bioclim vars
   names(wclim) <- c("bio1", "bio2", "bio3", "bio4", "bio5", "bio6", "bio7", "bio8", "bio9", "bio10",
                     "bio11", "bio12", "bio13", "bio14", "bio15", "bio16", "bio17", "bio18", "bio19")
-  wclim <- raster::stack(wclim)
 
   if (save_output == FALSE) {
-    unlink(folder, recursive=TRUE)
+    unlink(folder, recursive = TRUE)
   }
 
   return(wclim)
