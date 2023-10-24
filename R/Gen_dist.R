9e59009e75b1ace653b63c45845ed727e71751fd
E. Anne Chambers <eachambers@utexas.edu>
2023-10-24 12:22:32 -0600

    allow gen_dist to take in dosage matrix

--- R/Gen_dist.R
+++ R/Gen_dist.R
@@ -1,6 +1,6 @@
 #' Calculate genetic distances
 #'
-#' @param vcf path to vcf file or a `vcfR` type object
+#' @param gen path to vcf file, a `vcfR` type object, or a dosage matrix
 #' @param dist_type the type of genetic distance to calculate (options: `"euclidean"` (default), `"bray_curtis"`, `"dps"` for proportion of shared alleles, `"plink"`, or `"pc"` for PC-based)
 #' @param plink_file if `"plink"` dist_type is used, path to plink distance file (typically ".dist"; required only for calculating plink distance)
 #' @param plink_id_file if `"plink"` dist_type is used, path to plink id file (typically ".dist.id"; required only for calculating plink distance)
@@ -14,45 +14,42 @@
 #'
 #' @return pairwise distance matrix for given distance metric
 #' @export
-gen_dist <- function(vcf = NULL, dist_type = "euclidean", plink_file = NULL, plink_id_file = NULL, npc_selection = "auto", criticalpoint = 2.0234) {
-  # Import vcf if provided --------------------------------------------------
-  if (!is.null(vcf)) if (!inherits(vcf, "vcfR")) vcf <- vcfR::read.vcfR(vcf)
+gen_dist <- function(gen, dist_type = "euclidean", plink_file = NULL, plink_id_file = NULL,
+                     npc_selection = "auto", criticalpoint = 2.0234) {
+
+  # Process input data ------------------------------------------------------
+  # Read in vcf if path provided
+  if (is.character(gen)) gen <- vcfR::read.vcfR(gen)
+  # Convert vcf to dosage matrix
+  if (inherits(gen, "vcfR") & dist_type == "euclidean" | dist_type == "bray_curtis" | dist_type == "pc") gen <- vcf_to_dosage(gen)
 
   # Calculate Euclidean distances -------------------------------------------
   if (dist_type == "euclidean") {
-    # Convert to genlight and matrix
-    gl <- vcfR::vcfR2genlight(vcf)
-    mat <- as.matrix(gl)
-
     # Perform imputation with warning
-    if (any(is.na(mat))) {
-      mat <- simple_impute(mat, median)
+    if (any(is.na(gen))) {
+      gen <- simple_impute(gen, median)
       warning("NAs found in genetic data, imputing to the median (NOTE: this simplified imputation approach is strongly discouraged. Consider using another method of removing missing data)")
     }
 
-    dists <- ecodist::distance(mat, method = "euclidean")
+    dists <- ecodist::distance(gen, method = "euclidean")
     dists <- as.matrix(dists)
     return(as.data.frame(dists))
   }
 
   # Calculate Bray-Curtis distances -----------------------------------------
   if (dist_type == "bray_curtis") {
-    # Convert to genlight and matrix
-    gl <- vcfR::vcfR2genlight(vcf)
-    mat <- as.matrix(gl)
-
     # Perform imputation with warning
-    if (any(is.na(mat))) {
-      mat <- simple_impute(mat, median)
+    if (any(is.na(gen))) {
+      gen <- simple_impute(gen, median)
       warning("NAs found in genetic data, imputing to the median (NOTE: this simplified imputation approach is strongly discouraged. Consider using another method of removing missing data)")
     }
 
     # Check for NAs
-    if (any(is.na(mat))) {
+    if (any(is.na(gen))) {
       stop("NA values found in genetic data")
     }
 
-    dists <- ecodist::distance(mat, method = "bray-curtis")
+    dists <- ecodist::distance(gen, method = "bray-curtis")
     dists <- as.matrix(dists)
     return(as.data.frame(dists))
   }
@@ -60,7 +57,7 @@ gen_dist <- function(vcf = NULL, dist_type = "euclidean", plink_file = NULL, pli
   # Calculate proportion of shared alleles ----------------------------------
   if (dist_type == "dps") {
     # Convert to genind
-    genind <- vcfR::vcfR2genind(vcf)
+    genind <- vcfR::vcfR2genind(gen)
     dists <- adegenet::propShared(genind)
     return(as.data.frame(dists))
   }
@@ -79,19 +76,15 @@ gen_dist <- function(vcf = NULL, dist_type = "euclidean", plink_file = NULL, pli
 
   # PC-based dist -----------------------------------------------------------
   if (dist_type == "pc") {
-    # Convert to genlight
-    gl <- vcfR::vcfR2genlight(vcf)
-    mat <- as.matrix(gl) # to check for NAs
-
     # Perform imputation with warning
-    if (any(is.na(mat))) {
-      mat <- simple_impute(mat, median)
-      gl <- adegenet::as.genlight(mat)
+    if (any(is.na(gen))) {
+      gen <- simple_impute(gen, median)
+      gl <- adegenet::as.genlight(gen)
       warning("NAs found in genetic data, imputing to median (NOTE: this simplified imputation approach is strongly discouraged. Consider using another method of removing missing data)")
     }
 
     # Check for NAs
-    if (any(is.na(mat))) {
+    if (any(is.na(gen))) {
       stop("NA values found in genetic data")
     }
 
