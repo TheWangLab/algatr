b5afb168fb1dfb2b30ef8c4a7b31cab376d7315b
Anusha Bishop <46795148+AnushaPB@users.noreply.github.com>
2024-04-06 10:36:57 -0700

    Update setup.sh

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Modified from: https://github.com/boettiger-lab/nasa-topst-env-justice/blob/main/.devcontainer/setup.sh
+# Taken from: https://github.com/boettiger-lab/nasa-topst-env-justice/blob/main/.devcontainer/setup.sh
 sudo cp /etc/rstudio/disable_auth_rserver.conf /etc/rstudio/rserver.conf
 sudo sudo bash -c 'echo "USER=rstudio" >>/etc/environment'
 sudo /init &> /dev/null &
